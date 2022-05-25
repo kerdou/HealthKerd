1482a7b3fd0290ad688cabe99b17c28a78d1e8d9
kerdou <gautier.le.hir@gmail.com>
2022-05-25 18:44:47 +0200

    Mise à jour de la regex de numéro de tel.

--- dev/JS & TS/TS/services/regexStore/telRegex.ts
+++ dev/JS & TS/TS/services/regexStore/telRegex.ts
@@ -1,9 +1,11 @@
 export default class TelRegex
 {
     public telRegex(telValue: string): boolean {
-        /** ^                       Doit être placé au début du numéro de tel
-         *   (([. ]?[0-9]{2}){5}    Des séries de 2 chiffres séparées par des points ou sans séparation*/
-        let tel = '^([. ]?[0-9]{2}){5}';
+      /** ^                                 Doit être placé au début du numéro de tel
+       *   ^([0-9]{2}[. ]?){4}              4 séries de 2 chiffres séparées par un point ou sans séparation
+       *                      ([0-9]{2})$   Suivies de 2 chiffres
+       * */
+       let tel = "^([0-9]{2}[. ]?){4}([0-9]{2})$";
 
         let telConcat = tel; // Remplace par \ par des \\. Etape nécessaire avant de transformer la string en expression régulière.
         let telRegex = new RegExp(telConcat); // création du regex
--- src/Services/regexStore/TelRegex.php
+++ src/Services/regexStore/TelRegex.php
@@ -12,9 +12,11 @@ class TelRegex
     {
         $stringToCheck = html_entity_decode($stringToCheck);
 
-        /** ^                       Doit être placé au début du numéro de tel
-         *   (([. ]?[0-9]{2}){5}    Des séries de 2 chiffres séparées par des points ou sans séparation*/
-        $tel = '^([. ]?[0-9]{2}){5}';
+        /** ^                                 Doit être placé au début du numéro de tel
+         *   ^([0-9]{2}[. ]?){4}              4 séries de 2 chiffres séparées par un point ou sans séparation
+         *                      ([0-9]{2})$   Suivies de 2 chiffres
+         * */
+        $tel = "^([0-9]{2}[. ]?){4}([0-9]{2})$";
 
         $pregListForTel = "/" . $tel . "/";
         $regexResult = (preg_match($pregListForTel, $stringToCheck) ? true : false);
